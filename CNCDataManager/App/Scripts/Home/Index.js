23943ea1385f0a0a6e8e7e918ef94f3e3af19279
billshooting <bill_shooting@sina.com>
2016-05-07 17:37:45 +0800

    主页修改3

---
+++ CNCDataManager/App/Scripts/Home/Index.js
@@ -0,0 +1,11 @@
+﻿
+$(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() < 10) {
+            $("#navbar").animate({ background: 'none' });
+        }
+        else {
+            $("#navbar").animate({ background: 'black' });
+        }
+    });
+});
