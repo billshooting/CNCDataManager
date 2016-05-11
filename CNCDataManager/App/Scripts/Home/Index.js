f9e726061608933e6ad5440fdcc5131513062078
billshooting <bill_shooting@sina.com>
2016-05-11 09:50:28 +0800

    图片翻转尝试

--- CNCDataManager/App/Scripts/Home/Index.js
+++ CNCDataManager/App/Scripts/Home/Index.js
@@ -1,4 +1,17 @@
 ﻿
+var deg = 0, rotateIntval;
+function startRotate(img) {
+    deg += 5;
+    img.css({ transform: "rotateY(" + deg + "deg)" });
+    //img.css({ webkitTransform: "rotateY(" + deg + "deg)" });
+    //img.css({ OTransform: "rotateY(" + deg + "deg)" });
+    //img.css({ MozTransform: "rotateY(" + deg + "deg)" });
+    if (deg == 360) {
+        clearInterval(rotateIntval);
+        deg = 0;
+    }
+}
+
 $(function () {
     $(window).scroll(function () {
         if ($(window).scrollTop() < 10) {
@@ -15,5 +28,13 @@ $(function () {
     $("#back-to-top").click(function () {
         $('body,html').animate({ scrollTop: 0 }, 300);
         return false;
+    });
+
+    //实现图片翻转功能  
+    $(".tech-img").mouseover(function () {
+        var img = $(this);
+        img.animate({ transform: "rotateY(180deg)", height: "+=25" }, 1000).animate({ height: "-=25" }, 1000);
+        //clearInterval(rotateIntval);
+        //rotateIntval = setInterval(startRotate(img), 50);
     });
 });
