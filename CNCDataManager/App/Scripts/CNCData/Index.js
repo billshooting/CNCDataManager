2eb0ccc5f30ab44653c7a3b1da5a110503d9bd66
billshooting <bill_shooting@sina.com>
2016-05-18 18:43:37 +0800

    数据主页1

--- CNCDataManager/App/Scripts/CNCData/Index.js
+++ CNCDataManager/App/Scripts/CNCData/Index.js
@@ -1,1 +1,9 @@
 ﻿
+$(function () {
+    $("#desc-header-1").click(function () {
+        $(this).removeClass("desc-header").addClass("desc-header-open");
+        $("#desc-body-1").animate({ height: 'toggle' }, 1000, function () {
+            $(this).removeClass("desc-body").addClass("desc-body-open");
+        });
+    });
+});
