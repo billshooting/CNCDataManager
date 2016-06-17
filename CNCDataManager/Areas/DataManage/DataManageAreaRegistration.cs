5e2c2f17e1659205d72a5c45d93409b7793b7b63
顾星炜Bill <bill_shooting@sina.com>
2016-06-17 16:22:25 +0800

    为CNCDataManager添加区域
    1.添加Selection区域，用于选型的开发；
    2.添加DataManage区域，用于迁移数据库部分；

---
+++ CNCDataManager/Areas/DataManage/DataManageAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace CNCDataManager.Areas.DataManage
+{
+    public class DataManageAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "DataManage";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "DataManage_default",
+                "DataManage/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ CNCDataManager/Areas/Selection/SelectionAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace CNCDataManager.Areas.Selection
+{
+    public class SelectionAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Selection";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Selection_default",
+                "Selection/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
