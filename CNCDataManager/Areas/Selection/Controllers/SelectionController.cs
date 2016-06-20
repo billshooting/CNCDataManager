7a9ca1d7990b9f5d1b369c2fb5e5d219d502d9df
jiangchangzzz <1015312525@qq.com>
2016-06-20 22:39:14 +0800

    加载angular相关库
    加载angularui-route，ng-grid库

---
+++ CNCDataManager/Areas/Selection/Controllers/SelectionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CNCDataManager.Areas.Selection.Controllers
+{
+    public class SelectionController : Controller
+    {
+        // GET: Selection/Selection
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
