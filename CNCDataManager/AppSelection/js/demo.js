a1565840f722a8dd39378ff40845cea0e149da36
jiangchangzzz <1015312525@qq.com>
2016-06-28 20:28:17 +0800

    Revert "更新"
    This reverts commit 7aef62958a9ca86d00f428b8845ccac5ac4fa72d.

--- CNCDataManager/AppSelection/js/demo.js
+++
@@ -1,8 +0,0 @@
-﻿$(function ($) {
-  $('.spinner .btn:first-of-type').on('click', function() {
-    $('.spinner input').val( parseInt($('.spinner input').val(), 10) + 1);
-  });
-  $('.spinner .btn:last-of-type').on('click', function() {
-    $('.spinner input').val( parseInt($('.spinner input').val(), 10) - 1);
-  });
-});
