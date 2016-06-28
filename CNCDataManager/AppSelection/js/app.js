8d143d9a81c4039ea977fd2fdff21bd701339b13
jiangchangzzz <1015312525@qq.com>
2016-06-28 20:34:06 +0800

    Revert "Revert "更新""
    This reverts commit a1565840f722a8dd39378ff40845cea0e149da36.

---
+++ CNCDataManager/AppSelection/js/app.js
@@ -0,0 +1,63 @@
+﻿var selectionApp = angular.module("selectionApp", ["ui.router", "controllers"]);
+selectionApp.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/CNCType');
+    $stateProvider
+        .state('CNCType', {
+            url: '/CNCType',
+            views: {
+                '': {
+                    templateUrl: '../../../../AppSelection/tpls/home.html'
+                },
+                'list@CNCType': {
+                    templateUrl: '../../../../AppSelection/tpls/CNCType/CNCTypeList.html'
+                },
+                'table@CNCType': {
+                    templateUrl: '../../../../AppSelection/tpls/CNCType/CNCTypeTable.html'
+                }
+            }
+        })
+      .state('CNCType.WorkingCondition',{
+            url: '/WorkingCondition',
+            views:{
+                    'table@CNCType': {
+                        templateUrl: '../../../../AppSelection/tpls/CNCType/CNCWorkingCondition.html'
+                    }
+             }
+        })
+        .state('CNCSystem', {
+            url: '/CNCSystem',
+            views: {
+                '': {
+                    templateUrl: '../../../../AppSelection/tpls/home.html'
+                },
+                'list@CNCSystem': {
+                    templateUrl: '../../../../AppSelection/tpls/CNCSystem/CNCSystemList.html'
+                },
+                'table@CNCSystem': {
+                    templateUrl: '../../../../AppSelection/tpls/CNCSystem/CNCSystemTable.html'
+                }
+            }
+        })
+        .state('CNCSystem.Accessories', {
+            url: '/Accessories',
+            views: {
+                'table@CNCSystem': {
+                    templateUrl: '../../../../AppSelection/tpls/CNCSystem/CNCSystemAccessories.html'
+                }
+            }
+        })
+        .state('FeedSystem', {
+            url: '/FeedSystem',
+            views: {
+                '': {
+                    templateUrl: '../../../../AppSelection/tpls/home.html'
+                },
+                'list@FeedSystem': {
+                    templateUrl: '../../../../AppSelection/tpls/FeedSystem/FeedSystemList.html'
+                },
+                'table@FeedSystem':{
+                    templateUrl: '../../../../AppSelection/tpls/FeedSystem/LinearRollingGuide/LinearRollingGuideTable.html'
+                }
+            }
+        })
+});
---
+++ CNCDataManager/AppSelection/js/controllers.js
@@ -0,0 +1,15 @@
+﻿var controllers = angular.module('controllers', []);
+controllers.controller('CNCWorkingConditionCtrl', function ($scope) {
+    $scope.submitForm = function (isValid) {
+        if (isValid) {
+            alert('yes');
+        }
+    };
+});
+
+controllers.controller('CNCSystemTable', function ($scope,$http) {
+    $http.get('././././App_Data/NCSystem.json')
+      .success(function (response) {
+          $scope.NCSystems = response.data;
+      });
+});
